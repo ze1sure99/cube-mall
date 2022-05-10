6ed32e7a429e36f9bb379fe66b55262f083014a2
zzxcccccc <1306445936@qq.com>
2022-05-10 22:29:22 +0800

    Initial commit

---
+++ cube-goods/src/main/java/com/ze1sure99/cubegoods/CubeGoodsApplication.java
@@ -0,0 +1,13 @@
+package com.ze1sure99.cubegoods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubeGoodsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CubeGoodsApplication.class, args);
+    }
+
+}
---
+++ cube-goods/src/test/java/com/ze1sure99/cubegoods/CubeGoodsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ze1sure99.cubegoods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubeGoodsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
